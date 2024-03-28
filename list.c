3806c087a33a4578f0042a7edb88ff9f90dbb858
Your Name <you@example.com>
2024-03-28 21:12:11 +0000

    new try

--- list.c
+++ list.c
@@ -106,14 +106,15 @@ void * popCurrent(List * list) {
   void* data = list->current->data;
   Node* izq = list->current->prev;
   Node* der = list->current->next;
-  izq->next = der;
-  der->prev = izq;
+  if(izq) izq->next = der;
+  if(der) der->prev = izq;
+  
   if(list->current == list->head) list->head = der;
   if(list->current == list->tail) list->tail = izq;
-  
   free(list->current);
   list->current = NULL;
   return data;
+  
 }
 
 void cleanList(List * list) {
